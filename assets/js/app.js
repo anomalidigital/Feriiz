6b9f134e6f73affbde416beb7f891eee92374527
Baldy Feriiz <baldy@feriiz.local>
2026-08-05 14:02:31 +0700

    fix: persist authentication in local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
 /* === Auth Guard === */
 (function() {
-    if (!sessionStorage.getItem('feriiz_user')) {
+    if (!localStorage.getItem('feriiz_user')) {
         window.location.href = 'login.html';
     }
 })();
 
 function feriizLogout() {
-    sessionStorage.removeItem('feriiz_user');
+    localStorage.removeItem('feriiz_user');
     window.location.href = 'login.html';
 }
 
